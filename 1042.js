508267c7084693070c2b1a5e487da46df2992c8a
Matheus Togni <matheustogni64@gmail.com.br>
2024-09-27 17:18:11 -0300

    att

--- 1042.js
+++ 1042.js
@@ -1,2 +1,10 @@
 var input = require('fs').readFileSync('./dev/stdin/file.txt', 'utf8');
 var lines = input.split('\n');
+
+let lenha = (line) => line.split(" ").map(a => parseFloat(a));
+let [a, b, c] = lenha(lines.shift());
+
+let geral = [a,b,c]
+
+geral.sort()
+console.log(geral)
