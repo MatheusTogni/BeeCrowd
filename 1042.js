7dc463d0b5b30313ffc03c6ca335e059130b3e85
Matheus Togni <matheustogni64@gmail.com.br>
2024-09-27 17:28:25 -0300

    quase

--- 1042.js
+++ 1042.js
@@ -5,6 +5,10 @@ let lenha = (line) => line.split(" ").map(a => parseFloat(a));
 let [a, b, c] = lenha(lines.shift());
 
 let geral = [a,b,c]
-
 geral.sort()
-console.log(geral)
+let geral2 = [a,b,c]
+
+for (let i = 0; i < geral.length; i++) {
+    console.log(geral[i])
+    console.log(geral2[i])
+}
