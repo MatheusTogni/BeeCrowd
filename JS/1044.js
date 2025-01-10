d4e2d08f83b8688a24048b1cbae450af4a651556
Matheus Togni <matheustogni64@gmail.com>
2025-01-10 11:55:12 -0300

    att

--- JS/1044.js
+++ JS/1044.js
@@ -3,3 +3,9 @@ var lines = input.split('\n');
 
 let lenha = (line) => line.split(" ").map(a => parseInt(a));
 let [a, b] = lenha(lines.shift());
+
+if (a % b === 0 || b % a === 0) {
+    console.log('Sao Multiplos')
+} else {
+    console.log('Nao sao Multiplos')
+}
